789600635b3719e2a900965fd66583b88954cebb
cjackal <44624812+cjackal@users.noreply.github.com>
2020-03-19 04:19:00 -0700

    EM algorithm now train log probabilities

--- HMM.py
+++ HMM.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.special import logsumexp
 import random
 
 class HiddenMarkovModel:
@@ -42,9 +43,12 @@ class HiddenMarkovModel:
 
         self.L = len(A)
         self.D = len(O[0])
-        self.A = A
-        self.O = O
-        self.A_start = [1. / self.L for _ in range(self.L)]
+        self.A = np.asarray(A)
+        self.O = np.asarray(O)
+        self.A_start = np.ones(self.L)/self.L
+        self.A_log = np.log(self.A)
+        self.O_log = np.log(self.O)
+        self.A_start_log = np.log(self.A_start)
 
 
     def viterbi(self, x):
@@ -69,31 +73,30 @@ class HiddenMarkovModel:
         #
         # For instance, probs[1][0] is the probability of the prefix of
         # length 1 ending in state 0.
-        probs = [[0. for _ in range(self.L)] for _ in range(M + 1)]
-        seqs = [['' for _ in range(self.L)] for _ in range(M + 1)]
+        logprobs = np.zeros((M+1, self.L))
+        seqs = np.asarray([['' for _ in range(self.L)] for _ in range(M + 1)])
 
-        for st in range(self.L):
-            probs[1][st] = self.A_start[st] * self.O[st][x[0]]
-            seqs[1][st] = str(st)
+        logprobs[1] = self.A_start_log + self.O_log[:, x[0]]
+        seqs[1] = np.array([str(st) for st in range(self.L)])
 
         for t in range(1, M):
             for st in range(self.L):
                 max_tr_prob = 0
                 max_prev_st = 0
                 for prev_st in range(self.L):
-                    tr_prob = probs[t][prev_st]*self.A[prev_st][st]
+                    tr_prob = logprobs[t, prev_st] + self.A_log[prev_st, st]
                     if tr_prob > max_tr_prob:
                         max_tr_prob = tr_prob
                         max_prev_st = prev_st
-                max_prob = max_tr_prob * self.O[st][x[t]]
-                probs[t+1][st] = max_prob
-                seqs[t+1][st] = seqs[t][max_prev_st] + str(max_prev_st)
+                max_prob = max_tr_prob + self.O_log[st, x[t]]
+                logprobs[t+1, st] = max_prob
+                seqs[t+1, st] = seqs[t, max_prev_st] + str(max_prev_st)
 
-        max_idx = probs[M].index(max(probs[M]))
+        max_idx = logprobs[M].index(max(logprobs[M]))
         max_seq = seqs[M][max_idx]
         return max_seq
 
-
+ 
     def forward(self, x, normalize=False):
         '''
         Uses the forward algorithm to calculate the alpha probability
@@ -120,29 +123,20 @@ class HiddenMarkovModel:
         '''
 
         M = len(x)      # Length of sequence.
-        alphas = [[0. for _ in range(self.L)] for _ in range(M + 1)]
+        alphas = np.zeros((M+1, self.L))
 
-        denom = 0
-        for st in range(self.L):
-            alphas[1][st] = self.A_start[st]*self.O[st][x[0]]
-            if normalize:
-                denom += alphas[1][st]
+        alphas[1] = self.A_start_log + self.O_log[:, x[0]]
         if normalize:
-            alphas[1][st] = alphas[1][st] / denom
+            alphas[1] -= logsumexp(alphas[1])
 
         for t in range(1, M):
-            denom = 0
             for st in range(self.L):
-                prev_prob = 0
-                for prev_st in range(self.L):
-                    prev_prob += alphas[t][prev_st]*self.A[prev_st][st]
-                alphas[t+1][st] = prev_prob*self.O[st][x[t]]
-                if normalize:
-                    denom += alphas[t+1][st]
+                prev_logprob = logsumexp(alphas[t, :]+self.A_log[:, st])
+                alphas[t+1, st] = prev_logprob + self.O_log[st, x[t]]
             if normalize:
-                alphas[t+1][st] = alphas[t+1][st] / denom
+                alphas[t+1] -= logsumexp(alphas[t+1])
 
-        return alphas
+        return alphas ### alphas[0] is zero vector
 
 
     def backward(self, x, normalize=False):
@@ -171,82 +165,22 @@ class HiddenMarkovModel:
         '''
 
         M = len(x)      # Length of sequence.
-        betas = [[0. for _ in range(self.L)] for _ in range(M + 1)]
+        betas = np.zeros((M+1, self.L))
 
-        denom = 0
-        for st in range(self.L):
-            betas[M][st] = 1    
-            if normalize:
-                denom += betas[M][st]
         if normalize:
-            betas[M][st] = betas[M][st] / denom
+            betas[M] = np.log(np.ones(self.L)/self.L)
 
         for t in reversed(range(1, M+1)):
-            denom = 0
             for st in range(self.L):
-                next_prob = 0
-                for next_st in range(self.L):
-                    next_prob += betas[t][next_st]*self.A[st][next_st]*self.O[next_st][x[t-1]]
-                betas[t-1][st] = next_prob
-                if normalize:
-                    denom += betas[t-1][st]
+                next_logprob = logsumexp(betas[t, :]+self.A_log[st, :]+self.O_log[:, x[t-1]])
+                betas[t-1, st] = next_logprob
             if normalize:
-                betas[t-1][st] = betas[t-1][st] / denom
+                betas[t-1] -= logsumexp(betas[t-1])
 
         return betas
 
 
-    def supervised_learning(self, X, Y):
-        '''
-        Trains the HMM using the Maximum Likelihood closed form solutions
-        for the transition and observation matrices on a labeled
-        datset (X, Y). Note that this method does not return anything, but
-        instead updates the attributes of the HMM object.
-
-        Arguments:
-            X:          A dataset consisting of input sequences in the form
-                        of lists of variable length, consisting of integers 
-                        ranging from 0 to D - 1. In other words, a list of
-                        lists.
-
-            Y:          A dataset consisting of state sequences in the form
-                        of lists of variable length, consisting of integers 
-                        ranging from 0 to L - 1. In other words, a list of
-                        lists.
-
-                        Note that the elements in X line up with those in Y.
-        '''
-
-        # Calculate each element of A using the M-step formulas.
-
-        Count_denom = [0 for _ in range(self.L)]
-        for a in range(self.L):
-            num_ab = [0 for _ in range(self.L)]
-            for j in range(len(Y)):
-                for i in range(len(Y[j])-1):
-                    if Y[j][i]==a:
-                        Count_denom[a] += 1
-                        for b in range(self.L):
-                            if Y[j][i+1]==b:
-                                num_ab[b] += 1
-            self.A[a] = [num_ab[b] / Count_denom[a] for b in range(self.L)]
-
-        # Calculate each element of O using the M-step formulas.
-
-        Count_denom = [0 for _ in range(self.L)]
-        for a in range(self.L):
-            num_aw = [0 for _ in range(self.D)]
-            for j in range(len(X)):
-                for i in range(len(X[j])):
-                    if Y[j][i]==a:
-                        Count_denom[a] += 1
-                        for w in range(self.D):
-                            if X[j][i]==w:
-                                num_aw[w] += 1
-            self.O[a] = [num_aw[w] / Count_denom[a] for w in range(self.D)]
-
-
-    def unsupervised_learning(self, X, N_iters):
+    def unsupervised_learning(self, X, N_iters, threshold=0.01, verbose=False):
         '''
         Trains the HMM using the Baum-Welch algorithm on an unlabeled
         datset X. Note that this method does not return anything, but
@@ -259,32 +193,54 @@ class HiddenMarkovModel:
 
             N_iters:    The number of iterations to train on.
         '''
-
+        if verbose:
+            print('Train unsupervised HMM:')
         for _ in range(N_iters):
-            A_num = [[0 for _ in range(self.L)] for _ in range(self.L)]
-            A_denom = [[0 for _ in range(self.L)] for _ in range(self.L)]
-            O_num = [[0 for _ in range(self.D)] for _ in range(self.L)]
-            O_denom = [[0 for _ in range(self.D)] for _ in range(self.L)]
+            if ((_+1) % (N_iters//10) == 0) and verbose:
+                print('.', end='')
+            A_num = np.zeros((self.L, self.L))
+            A_denom = np.zeros(self.L)
+            O_num = np.zeros((self.L, self.D))
+            O_denom = np.zeros(self.L)
+
+            A_log_prev = self.A_log
+            O_log_prev = self.O_log
+
             for j in range(len(X)):
-                alphas, betas = self.forward(X[j], normalize=True)[1:], self.backward(X[j], normalize=True)[1:]
+                alphas, betas = self.forward(X[j], normalize=True)[1:,:], self.backward(X[j], normalize=True)[1:,:]
+                logprob_curr = np.zeros((len(X[j]), self.L))
+                logprob_curr_nxt = np.zeros((len(X[j])-1, self.L, self.L))
                 for i in range(len(X[j])):
-                    if i != len(X[j]) - 1:
-                        A_num_norm = sum(sum(alphas[i][c]*betas[i+1][d]*self.A[c][d]*self.O[d][X[j][i+1]] for c in range(self.L)) for d in range(self.L))
-                    A_denom_norm = sum(alphas[i][c]*betas[i][c] for c in range(self.L))
-                    for a in range(self.L):
-                        for b in range(self.L):
-                            if i != len(X[j]) - 1:
-                                A_num[a][b] += alphas[i][a]*betas[i+1][b]*self.A[a][b]*self.O[b][X[j][i+1]] / A_num_norm
-                                A_denom[a][b] += alphas[i][a]*betas[i][a] / A_denom_norm
-                        for w in range(self.D):
-                            O_denom[a][w] += alphas[i][a]*betas[i][a] / A_denom_norm
+                    logprob_curr[i] = alphas[i,:]+betas[i,:]
+                    logprob_curr[i] -= logsumexp(logprob_curr[i])
+                    if i != len(X[j])-1:
+                        logprob_curr_nxt[i] = self.A_log+alphas[i,:].reshape((self.L,1))+betas[i+1,:]+self.O_log[:,X[j][i+1]]
+                        logprob_curr_nxt[i] -= logsumexp(logprob_curr_nxt[i])
+                for a in range(self.L):
+                    A_denom[a] = np.logaddexp(A_denom[a], logsumexp(logprob_curr[:-1,a]))
+                    O_denom[a] = np.logaddexp(O_denom[a], logsumexp(logprob_curr[:,a]))
+                    for b in range(self.L):
+                        A_num[a,b] = np.logaddexp(A_num[a,b], logsumexp(logprob_curr_nxt[:,a,b]))
+                    for w in range(self.D):
+                        logprob_w = []
+                        for i in range(len(X[j])):
                             if X[j][i]==w:
-                                O_num[a][w] += alphas[i][a]*betas[i][a] / A_denom_norm
-            for a in range(self.L):
-                for b in range(self.L):
-                    self.A[a][b] = A_num[a][b] / A_denom[a][b]
-                for w in range(self.D):
-                    self.O[a][w] = O_num[a][w] / O_denom[a][w]
+                                logprob_w.append(logprob_curr[i,a])
+                        O_num[a,w] = np.logaddexp(O_num[a,w], np.logaddexp.reduce(logprob_w))
+
+            self.A_log = A_num - A_denom.reshape((self.L,1))
+            self.O_log = O_num - O_denom.reshape((self.L,1))
+
+            delta_A = np.abs(self.A_log - A_log_prev)
+            delta_O = np.abs(self.O_log - O_log_prev)
+
+            if np.amax(delta_A*np.exp(A_log_prev))+np.amax(delta_O*np.exp(O_log_prev)) < threshold:
+                if verbose:
+                    print('Converged at', _, 'iterations')
+                break
+
+        self.A = np.exp(self.A_log)
+        self.O = np.exp(self.O_log)
 
 
     def generate_emission(self, M):
@@ -404,20 +360,18 @@ class HiddenMarkovModel:
         return prob
 
     def generate_sonnet(self, df, numLines=14):
-        idxtoword = df.index
-
-        A = np.asarray(self.A)
+        A = (self.A).copy()
         A_sum = A.sum(axis=1, keepdims=True)
         A = A/A_sum
-        O = np.asarray(self.O)
+        O = (self.O).copy()
         O_sum = O.sum(axis=1, keepdims=True)
         O = O/O_sum
-        A_start = np.asarray(self.A_start)
+        A_start = (self.A_start).copy()
         A_start_sum = np.sum(A_start)
         A_start = A_start/A_start_sum
 
         sonnet = []
-        for s in range(numLines):
+        for _ in range(numLines):
             line = []
             maxsyllensum = 0
             minsyllensum = 0
@@ -437,7 +391,6 @@ class HiddenMarkovModel:
                         minsyllensum += length1
                         maxsyllensum += max(length1, length2)
                     else:
-                        aretwosyllen = False
                         if isend1==True:
                             minsyllensum += length2
                             maxsyllensum += length2
@@ -452,61 +405,7 @@ class HiddenMarkovModel:
         return sonnet
 
 
-
-def supervised_HMM(X, Y):
-    '''
-    Helper function to train a supervised HMM. The function determines the
-    number of unique states and observations in the given data, initializes
-    the transition and observation matrices, creates the HMM, and then runs
-    the training function for supervised learning.
-
-    Arguments:
-        X:          A dataset consisting of input sequences in the form
-                    of lists of variable length, consisting of integers 
-                    ranging from 0 to D - 1. In other words, a list of lists.
-
-        Y:          A dataset consisting of state sequences in the form
-                    of lists of variable length, consisting of integers 
-                    ranging from 0 to L - 1. In other words, a list of lists.
-                    Note that the elements in X line up with those in Y.
-    '''
-    # Make a set of observations.
-    observations = set()
-    for x in X:
-        observations |= set(x)
-
-    # Make a set of states.
-    states = set()
-    for y in Y:
-        states |= set(y)
-    
-    # Compute L and D.
-    L = len(states)
-    D = len(observations)
-
-    # Randomly initialize and normalize matrix A.
-    A = [[random.random() for i in range(L)] for j in range(L)]
-
-    for i in range(len(A)):
-        norm = sum(A[i])
-        for j in range(len(A[i])):
-            A[i][j] /= norm
-    
-    # Randomly initialize and normalize matrix O.
-    O = [[random.random() for i in range(D)] for j in range(L)]
-
-    for i in range(len(O)):
-        norm = sum(O[i])
-        for j in range(len(O[i])):
-            O[i][j] /= norm
-
-    # Train an HMM with labeled data.
-    HMM = HiddenMarkovModel(A, O)
-    HMM.supervised_learning(X, Y)
-
-    return HMM
-
-def unsupervised_HMM(X, n_states, N_iters):
+def unsupervised_HMM(X, n_states, N_iters, threshold=0.01, verbose=False):
     '''
     Helper function to train an unsupervised HMM. The function determines the
     number of unique observations in the given data, initializes
@@ -561,6 +460,6 @@ def unsupervised_HMM(X, n_states, N_iters):
 
     # Train an HMM with unlabeled data.
     HMM = HiddenMarkovModel(A, O)
-    HMM.unsupervised_learning(X_reidxd, N_iters)
+    HMM.unsupervised_learning(X_reidxd, N_iters, threshold=threshold, verbose=verbose)
 
     return HMM, obs_idx
